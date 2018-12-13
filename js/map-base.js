82232b5d4db03e6b6f42f5d093995b606887ba3e
Fernando Cagua <efcaguab@gmail.com>
2018-12-13 16:28:14 +1300

    Search zoom control to bottom right

--- js/map-base.js
+++ js/map-base.js
@@ -2,7 +2,9 @@
 
 // Create a blank map and place it in the div with id="map"
 // Set the default position and zoom of the map
-var map = L.map("map").setView([-41.28666552, 174.772996908], 14);
+var map = L.map("map", {
+  zoomControl: false
+}).setView([-41.28666552, 174.772996908], 14);
 
 // Create a new layer for the map for the tiles
 L.tileLayer(
@@ -11,9 +13,15 @@ L.tileLayer(
     attribution:
       'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: "abcd",
-    minZoom: 11, // zooms have been chosen match the ranges of suburb sizes
-    maxZoom: 15,
+    minZoom: 10, // zooms have been chosen match the ranges of suburb sizes
+    maxZoom: 17,
     ext: "png"
   }
 ).addTo(map); // add the recently created layer to the blank map we created before
 // We have now a base map with tiles
+
+L.control
+  .zoom({
+    position: "bottomright"
+  })
+  .addTo(map);
