86c6a6c1e37aecc336b6096d9d2edb6c9fbeae6d
Fernando Cagua <efcaguab@gmail.com>
2018-12-14 10:52:11 +1300

    Submit button uses favicon

--- js/map-submit-button.js
+++ js/map-submit-button.js
@@ -1,4 +1,19 @@
-var send_button = L.easyButton("xxx", function(control) {
-  alert('"expensive query here"');
-});
+var send_button = L.easyButton(
+  "fa-paper-plane",
+  function(control) {
+    alert('"suburb info sent"');
+  },
+  "send data",
+  {
+    position: "bottomright"
+  }
+);
 send_button.addTo(map);
+
+// send_bu.on("zoomend", function(e) {
+//   if (e.target.getZoom() > 13) {
+//     findCoffee.enable();
+//   } else {
+//     findCoffee.disable();
+//   }
+// });
