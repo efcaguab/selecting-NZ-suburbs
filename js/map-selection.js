419a67e64c6411f7e518baa62610ef5b109fb59a
Fernando Cagua <efcaguab@gmail.com>
2018-12-13 16:31:36 +1300

    update selected suburbs on click

--- js/map-selection.js
+++ js/map-selection.js
@@ -1,12 +1,34 @@
 suburb_layer.on("click", select_unselect);
 
+var selected_style = {
+  fillColor: "darksalmon",
+  fillOpacity: 0.5,
+  strokeColor: "indianred"
+};
+
+var selected_suburbs = [];
+
 function select_unselect(x) {
   var feature_layer = x.layer;
   if (feature_layer.feature.properties.selected) {
     feature_layer.feature.properties.selected = false;
-    feature_layer.setStyle({ color: "black" });
+    feature_layer.setStyle(default_style);
+    remove_selected_suburb(feature_layer);
+    info.update();
   } else {
     feature_layer.feature.properties.selected = true;
-    feature_layer.setStyle({ color: "green" });
+    feature_layer.setStyle(selected_style);
+    add_selected_suburb(feature_layer);
+    info.update();
   }
+}
+
+function add_selected_suburb(feature_layer) {
+  selected_suburbs.push(feature_layer.feature.properties.SA22018_V1_00_NAME);
+}
+
+function remove_selected_suburb(feature_layer) {
+  selected_suburbs = selected_suburbs.filter(
+    x => x != feature_layer.feature.properties.SA22018_V1_00_NAME
+  );
 }
