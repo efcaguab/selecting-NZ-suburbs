a474d50fb41b565d7c67223ba23cd9c153ab1fbe
Fernando Cagua <efcaguab@gmail.com>
2018-12-14 10:52:29 +1300

    cosmetic changes

--- js/map-tooltips.js
+++ js/map-tooltips.js
@@ -4,6 +4,7 @@ suburb_layer
     return suburb_name;
   })
   .addTo(map);
+
 // suburb_layer
 //   .onEachFeature(function (this_layer) {
 //     var suburb_name = this_layer.feature.properties.SA22018_V1_00_NAME;
